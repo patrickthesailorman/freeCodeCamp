a9a71a812cdd72500501eac4f35ea9d5ac38ac39
Patrick McCarty <patrickthesailorman@gmail.com>
2017-12-29 13:31:47 +0000

    Construct strings with variables

---
+++ js/ConstructingStringsWithVariables.js
@@ -0,0 +1,9 @@
+
+// Example
+var ourName = "Free Code Camp";
+var ourStr = "Hello, our name is " + ourName + ", how are you?";
+
+// Only change code below this line
+var myName = "Patrick";
+var myStr = "My name is " + myName + " and I am swell!";
+
