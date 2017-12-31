bdf67ea57052051b622e5be00c816ff20f7f4e02
Patrick McCarty <patrickthesailorman@gmail.com>
2017-12-31 15:46:08 +0000

    Nested Arrays

---
+++ js/NestOneArrayWithinAnotherArray.js
@@ -0,0 +1,7 @@
+
+// Example
+var ourArray = [["the universe", 42], ["everything", 101010]];
+
+// Only change code below this line.
+var myArray = [["Rolling Stones", 42], ["Jerry Garcia Band", 56]];
+
