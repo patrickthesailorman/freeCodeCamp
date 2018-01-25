2bd573cbbcc4ce1ab2a21d108c27e3e65c16b9ba
Patrick McCarty <patrickthesailorman@gmail.com>
2018-01-25 00:46:33 +0000

    else

---
+++ js/IntroducingElseStatements.js
@@ -0,0 +1,17 @@
+
+function testElse(val) {
+  var result = "";
+  // Only change code below this line
+  
+  if (val <= 5) {
+    result = "5 or Smaller";
+  } else {
+     result = "Bigger than 5";
+  }
+  
+  // Only change code above this line
+  return result;
+}
+
+// Change this value to test
+testElse(4);
