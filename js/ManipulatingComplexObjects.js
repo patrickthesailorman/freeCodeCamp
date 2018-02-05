8f03b8eb36bc895f8a2bd1401cbfd37059edc733
Patrick McCarty <patrickthesailorman@gmail.com>
2018-02-05 02:43:52 +0000

    manipulate objects

---
+++ js/ManipulatingComplexObjects.js
@@ -0,0 +1,22 @@
+
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [ 
+      "CS", 
+      "8T", 
+      "LP" ],
+    "gold": true
+  },
+  {  
+  "artist": "Rolling Stones",
+    "title": "Some Girls",
+    "release_year": 1978,
+    "formats": [ 
+      "CS", 
+      "8T", 
+      "LP" ]// Add record here
+  }
+];
