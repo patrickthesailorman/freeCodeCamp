e859fcc5849222b462f908cba0a4baf59fd84551
Patrick McCarty <patrickthesailorman@gmail.com>
2018-03-12 00:43:45 +0000

    Add condense with reduce

---
+++ object-oriented/CondenseArraysWithReduce.js
@@ -0,0 +1,8 @@
+var array = [4,5,6,7,8];
+var singleVal = 0;
+
+// Only change code below this line.
+
+singleVal = array.reduce(function(previousVal, currentVal) {
+  return previousVal + currentVal;
+}, 0);
