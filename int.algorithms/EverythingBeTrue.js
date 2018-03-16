afb0a8554ed5544cc6c8f9af7f323676f64d1ad7
Patrick McCarty <patrickthesailorman@gmail.com>
2018-03-16 02:41:06 +0000

    Add everything be true

---
+++ int.algorithms/EverythingBeTrue.js
@@ -0,0 +1,13 @@
+
+function truthCheck(collection, pre) {
+  // Is everyone being true?
+  
+  for (var i = 0; i < collection.length; i++) {
+    if (!collection[i][pre]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex");
