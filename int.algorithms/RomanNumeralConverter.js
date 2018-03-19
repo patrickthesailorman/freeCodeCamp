492315aabc5048e0cf16f5d65a7d979bf5e09d33
Patrick McCarty <patrickthesailorman@gmail.com>
2018-03-19 01:42:46 +0000

    WIP romans

--- int.algorithms/RomanNumeralConverter.js
+++ int.algorithms/RomanNumeralConverter.js
@@ -5,8 +5,8 @@
 // Return Roman Numeral
 
 function convertToRoman(num) {
-  var romans = ['M','D','C','L','X','V','I'];
-  var decimals = [1000, 500, 100, 50, 10, 5, 1];
+  var romans = [ 'M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I' ];
+  var decimals = [ 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 ];
   var romanNum = '';
   
   for(var i = 0; i < decimals.length; i++) {
