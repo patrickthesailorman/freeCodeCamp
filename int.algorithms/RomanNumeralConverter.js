146ae3ab733514970c629444e0f6a873913d80d0
Patrick McCarty <patrickthesailorman@gmail.com>
2018-03-19 00:05:59 +0000

    WIP romans

---
+++ int.algorithms/RomanNumeralConverter.js
@@ -0,0 +1,22 @@
+// Array of decimal numbers
+// Identify given number
+// Match number to its roman counterpart
+// For loop the number for every romans numeral match
+// Return Roman Numeral
+
+function convertToRoman(num) {
+  var romans = ['I','V','X','L','C','D','M'];
+  var decimals = [1, 5, 10, 50, 100, 500, 1000];
+  var romanNum = '';
+  
+  for(var i = 0; i < decimals.length; i++) {
+    while(i === num){
+      
+    }
+     
+      
+  }
+  return num;
+}
+
+convertToRoman(36);
