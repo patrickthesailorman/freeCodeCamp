e0dd8475c99361e9c4f3bb3b5d6ae92e3175f899
Patrick McCarty <patrickthesailorman@gmail.com>
2018-03-20 01:09:29 +0000

    WIP

--- int.algorithms/WherforeArtThou.js
+++ int.algorithms/WherforeArtThou.js
@@ -4,14 +4,16 @@ function whatIsInAName(collection, source) {
   var arr = [];
   // Only change code below this line
   var src = Object.keys(source);
-  for (var i = 0; i < collection.length; i ++) {
-    if (collection[i] == source ) {
-      arr = collection.filter(src);
+   return collection.filter(function (obj) {
+  for (var i = 0; i < src.length; i ++) {
+    if (obj.hasOwnProperty(src[i]) || obj[src[i]] == source[src[i]]) {
+      arr = arr.push(obj.hasOwnProperty(src[i]));
     }
-  }
-  
+    }
+   
   // Only change code above this line
   return arr;
+   }) ;
 }
 
 whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
