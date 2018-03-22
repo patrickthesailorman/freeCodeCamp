75f40d780af09e74fd16286c8d2a36f29755d15b
Patrick McCarty <patrickthesailorman@gmail.com>
2018-03-22 21:51:03 +0000

    Add smallest

--- int.algorithms/SmallestCommonMultiple.js
+++ int.algorithms/SmallestCommonMultiple.js
@@ -1,11 +1,13 @@
 
 function smallestCommons(arr) {
- // Create new array and add all values from greater to smaller from the
-  // original array.
+ // Create new array and add all values from greater to smaller from the original array.
      arr.sort(function(a, b) {
     return b - a;
   });
-
+  // Variables needed declared outside the loops.
+  var lcm = 0;
+  var loop = 1;
+  var n;
     
  
   
