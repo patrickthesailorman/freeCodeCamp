02ed605fdacc8e4da29327832887844c0271b961
Patrick McCarty <patrickthesailorman@gmail.com>
2018-03-22 21:14:49 +0000

    Add regex

--- int.algorithms/SpinalTapCase.js
+++ int.algorithms/SpinalTapCase.js
@@ -5,6 +5,7 @@ function spinalCase(str) {
    
    str = str.replace(/([a-z])([A-Z])/g, '$1 $2');
 
+return str.replace(regex, '-').toLowerCase();
 
   str = str.toLowerCase();
   for (var i = 0; i < str.length; i++ ) {
