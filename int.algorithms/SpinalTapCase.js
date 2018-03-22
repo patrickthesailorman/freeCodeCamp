bfa52e3f6eef176f650652e87320a03b136a2668
Patrick McCarty <patrickthesailorman@gmail.com>
2018-03-22 21:15:28 +0000

    remove old

--- int.algorithms/SpinalTapCase.js
+++ int.algorithms/SpinalTapCase.js
@@ -7,11 +7,5 @@ function spinalCase(str) {
 
 return str.replace(regex, '-').toLowerCase();
 
-  str = str.toLowerCase();
-  for (var i = 0; i < str.length; i++ ) {
-  str= str.replace(" ", "-");
-  }
-  return str;
-}
 
 spinalCase('This Is Spinal Tap');
