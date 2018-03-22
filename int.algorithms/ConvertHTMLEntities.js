629535a635fd968efc833715b287693f028f1b82
Patrick McCarty <patrickthesailorman@gmail.com>
2018-03-22 00:18:01 +0000

    Add ConertHTML

--- int.algorithms/ConvertHTMLEntities.js
+++ int.algorithms/ConvertHTMLEntities.js
@@ -1,13 +1,20 @@
 
 function convertHTML(str) {
   // &colon;&rpar;
-  var newStr = str.split("");
-  for (var i = 0; i < newStr.length; i++) {
-    if (newStr[i] === "&") {
-        newStr = newStr.replace( "&", "&amp;");
-        }
-  }
-  return newStr;
-}
+  var multiple = {
+        '&' : '&amp;',
+        '<' : '&lt;',
+        '>' : '&gt;',
+        '"' : '&quot;',
+        "'" : '&apos;'
+      };
+      for(var char in multiple) {
+        var before = char;
+        var after= multiple[char]; 
+        var pattern = new RegExp(before, 'g');
+        str = str.replace(pattern,after);    
+      }
+      return str;
+    }
 
 convertHTML("Dolce & Gabbana");
