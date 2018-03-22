8870e43ffa4ab983477c7a7e57b585e6e31af860
Patrick McCarty <patrickthesailorman@gmail.com>
2018-03-22 00:10:58 +0000

    Add sorted

--- int.algorithms/SortedUnion.js
+++ int.algorithms/SortedUnion.js
@@ -6,8 +6,10 @@ function uniteUnique(arr) {
   for (var i = 0; i < args.length; i++) {
     var args1 = arguments[i];
     for (var j = 0; j < args1.length; j++) {
-    if (args[i] !== args1[j]){
-      newArr.push(args[i]);
+      var args2 = args1[j];
+      
+    if (newArr.indexOf(args2) < 0){
+      newArr.push(args2);
     }
   }
   }
