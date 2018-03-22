a2267a5c25c16687f4197f9472c27deda9f1066e
Patrick McCarty <patrickthesailorman@gmail.com>
2018-03-22 21:13:18 +0000

    Add regex

--- int.algorithms/SpinalTapCase.js
+++ int.algorithms/SpinalTapCase.js
@@ -1,6 +1,7 @@
 function spinalCase(str) {
   // "It's such a fine line between stupid, and clever."
   // --David St. Hubbins
+   var regex = /\s+|_+/g;
   str = str.toLowerCase();
   for (var i = 0; i < str.length; i++ ) {
   str= str.replace(" ", "-");
