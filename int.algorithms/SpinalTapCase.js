26983773be27b5f8315dbdc7bba847e339c8c7ac
Patrick McCarty <patrickthesailorman@gmail.com>
2018-03-22 21:17:51 +0000

    Add comment

--- int.algorithms/SpinalTapCase.js
+++ int.algorithms/SpinalTapCase.js
@@ -1,6 +1,5 @@
 function spinalCase(str) {
-  // "It's such a fine line between stupid, and clever."
-  // --David St. Hubbins
+  // Create a variable for the white space and underscores.
    var regex = /\s+|_+/g;
    // Replace low-upper case to low-space-uppercase
    str = str.replace(/([a-z])([A-Z])/g, '$1 $2');
