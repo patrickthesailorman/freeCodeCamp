4cc447f11d661177b9f54bae06ef6555056ca813
Patrick McCarty <patrickthesailorman@gmail.com>
2018-03-23 00:47:20 +0000

    Add Int. solution

--- int.algorithms/SmallestCommonMultiple.js
+++ int.algorithms/SmallestCommonMultiple.js
@@ -1,31 +1,23 @@
 
 function smallestCommons(arr) {
-arr.sort(function(a, b) {
-    return b - a;
-  });
- // Create new array and add all values from greater to smaller from the original array.
-var newArr = [];
-  for (var i = arr[0]; i >= arr[1]; i--) {
-    newArr.push(i);
-  }
-  // Variables needed declared outside the loops.
-  var lcm = 0;
-  var loop = 1;
-  var n;
-   
-   // Run code while n is not the same as the array length.
-  do {
-    lcm = newArr[0] * loop * newArr[1];
-    for (n = 2; n < newArr.length; n++) {
-      if (lcm % newArr[n] !== 0) {
-        break;
-      }
+    var range = [];
+    for (var i = Math.max(arr[0], arr[1]); i >= Math.min(arr[0], arr[1]); i--) {
+    range.push(i);
     }
 
-    loop++;
-  } while (n !== newArr.length);
+    // can use reduce() in place of this block
+    var lcm = range[0];
+    for (i = 1; i < range.length; i++) {
+    var GCD = gcd(lcm, range[i]);
+    lcm = (lcm * range[i]) / GCD;
+    }
+    return lcm;
 
-  return lcm;
-} 
- 
+    function gcd(x, y) {    // Implements the Euclidean Algorithm
+    if (y === 0)
+        return x;
+    else
+        return gcd(y, x%y);
+    }
+}
   
