c5d46505c7142c88767bcc4d8b2f5ec047e125e9
Patrick McCarty <patrickthesailorman@gmail.com>
2018-03-24 19:52:31 +0000

    Add Steamroller

--- int.algorithms/Steamroller.js
+++ int.algorithms/Steamroller.js
@@ -1,7 +1,19 @@
 function steamrollArray(arr) {
   // I'm a steamroller, baby
-  var newArr = [];
-  return arr;
+ var newArr = [];
+  var flatten = function(arg){
+    if (!Array.isArray(arg)) {
+      newArr.push(arg);
+   } else {
+      for (var a in arg) {
+        flatten(arg[a]);
+      }
+    }
+  };
+
+  // Call the function for each element in the array
+  arr.forEach(flatten);
+  return newArr;
 }
 
 steamrollArray([1, [2], [3, [[4]]]]);
