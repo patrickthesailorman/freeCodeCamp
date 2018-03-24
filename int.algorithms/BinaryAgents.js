26ea154202a99e26b2caee9508046b066feea1c2
Patrick McCarty <patrickthesailorman@gmail.com>
2018-03-24 20:06:00 +0000

    Add Binary

--- int.algorithms/BinaryAgents.js
+++ int.algorithms/BinaryAgents.js
@@ -1,9 +1,10 @@
 function binaryAgent(str) {
+  str.split(" ");
   for (var i = 0; i < str.length; i++) {
     var intStr = str.charCodeAt(str[i]);
   
   for (var j = 0; j < intStr.length; j++ ) {
-    var newStr = intStr.fromCharCode(intStr[j]);
+    var newStr = newStr.push(intStr.fromCharCode(intStr[j]));
      return newStr;
   }
   }
